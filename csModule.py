72778a9f9f2179c5fa1dcd47fe92d5745e7a4982
Tantros <134191613+SewedPotato@users.noreply.github.com>
2023-05-23 15:20:05 +0800

    hecc

--- csModule.py
+++ csModule.py
@@ -1,4 +1,18 @@
 # wattodo
 
+sav = [] # savecode string
+
 class Save:
-	def __init__
+	def __init__(self):
+		# how
+		pass
+	def Save(self,givemedata,givemoredata):
+		global sav
+		self.gmd = givemedata
+		self.gmmd = givemoredata
+		sav.append(gmd)
+		sav.append("->")
+		sav.append(gmmd)
+		compiled = encode(sav)
+		print(compiled)
+		
